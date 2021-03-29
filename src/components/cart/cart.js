63a766c4898e7b9874df0944822a0eab9b20e0af
Rahat <rahat3286@gmail.com>
2021-03-29 14:39:02 +0600

    make some changes

--- src/components/cart/cart.js
+++ src/components/cart/cart.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const cart = (props) => {
+const Cart = (props) => {
     const cart = props.cart;
     // console.log(cart);
     // const total = cart.reduce((total, prd) => total + prd.price, 0);
@@ -30,7 +30,6 @@ const cart = (props) => {
         const precision = num.toFixed(2);
         return Number(precision);
     }
-
     return (
         <div>
             <h4 className="text-success">Order Summery</h4>
@@ -47,4 +46,62 @@ const cart = (props) => {
     );
 };
 
-export default cart;
+export default Cart;
+
+
+
+
+
+
+
+
+// import React from 'react';
+// import { Link } from 'react-router-dom';
+
+// const cart = (props) => {
+    // const cart = props.cart;
+    // // console.log(cart);
+    // // const total = cart.reduce((total, prd) => total + prd.price, 0);
+    // // const totalPrice = cart.reduce((total,prd) => total +prd.price, 0);
+    // let total = 0;
+    // for (let i = 0; i < cart.length; i++) {
+    //     const product = cart[i];
+    //     total = total + product.price * product.quantity;
+    // }
+
+    // let shipping = 0;
+    // if (total > 35) {
+    //     shipping = 0;
+    // }
+    // if (total > 15) {
+    //     shipping = 4.99;
+    // }
+    // else if (total > 0) {
+    //     shipping = 12.99;
+    // }
+
+    // const tax = total / 10;
+    // const grandTotal = (total + shipping + Number(tax)).toFixed(2);
+
+    // const formatNumber = num => {
+    //     const precision = num.toFixed(2);
+    //     return Number(precision);
+    // }
+
+//     return (
+//         <div>
+            // <h4 className="text-success">Order Summery</h4>
+            // <p>Items ordered:{cart.length}</p>
+            // <p>Product Price: {formatNumber(total)}</p>
+            // <p><small>Shipping Cost: {shipping}</small></p>
+            // <p><small>Tax + VAT: {tax}</small></p>
+            // <p>Total Price: {grandTotal} $</p>
+            // <br />
+            // {
+            //     props.children
+            // }
+//         </div>
+//     );
+// };
+
+// export default cart;
